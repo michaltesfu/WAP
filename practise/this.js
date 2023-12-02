508e0c8a6be8f1a0900e490883b53056db8ffb16
michaltesfu <ghebrekidanmichal@gmail.com>
2023-12-01 18:44:48 -0600

    change

---
+++ practise/this.js
@@ -0,0 +1,32 @@
+
+// function max(num1,num2){
+//     if(num1>num2){
+//        return num1
+//     }
+// else{
+// return num2
+// }
+// }
+// let result = max(2,3)
+// console.log(result);
+
+
+
+// function maxthree(num1,num2,num3){
+//     if (num1 > num2 &&  num1 > num3){
+//     return num1;
+//     }
+//     else if(num2 > num1 && num2 > num3) {
+//         return num2;
+//     }
+//    else
+//         return num3;
+// }
+// let result1 = maxthree(5,67,89);
+// console.log(result1)
+
+function isvowel(char){
+    return(a,e,i,o,u). include(char.lowercase)
+}
+let result = isvowel('a')
+console.log(result)
